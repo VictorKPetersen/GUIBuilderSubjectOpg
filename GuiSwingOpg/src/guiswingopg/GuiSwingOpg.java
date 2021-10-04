a688ad8f047b8784c310ca95e199d8b845a40668
VictorKPetersen <83222801+VictorKPetersen@users.noreply.github.com>
2021-10-04 12:23:36 +0200

    Startup

---
+++ GuiSwingOpg/src/guiswingopg/GuiSwingOpg.java
@@ -0,0 +1,16 @@
+package guiswingopg;
+
+import javax.swing.JFrame;
+
+
+public class GuiSwingOpg {
+
+
+    public static void main(String[] args) {
+        JFrame gui = new mainWindow();
+        
+        gui.setSize(500, 500);
+        
+        gui.setVisible(true);
+    }  
+}
